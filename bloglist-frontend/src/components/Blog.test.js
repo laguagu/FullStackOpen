d1ed22be6f9130fb5fe3d185b578dec846bbbf1f
laguagu <49702497+laguagu@users.noreply.github.com>
2023-09-06 16:56:51 +0300

    jest test step2

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,10 +1,9 @@
 /* eslint-disable */
-import React from "react";
+import React from "react"
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import Blog from "./Blog";
-import Togglable from "./Togglable";
-import blogService from "../services/blogs";
+import { render, screen, fireEvent } from '@testing-library/react'
+import Blog from "./Blog"
+import userEvent from '@testing-library/user-event'; // Lisää tämä
 
 test("render title,", () =>{
     const blog = {
@@ -15,8 +14,41 @@ test("render title,", () =>{
 
     const element = screen.getByText('Moi')
     expect(element).toBeDefined()
-    //screen.debug()
-
 })
 
-// Tee testi, joka varmistaa että blogin näyttävä komponentti renderöi blogin titlen
+test("render blog details when 'view' button is clicked", async () => {
+    const blog = {
+      title: "Moi",
+      author: "Matti",
+      url: "https://example.com",
+      likes: 5,
+
+      user: {
+        username: "testuser",
+        name: "Test User"
+      }
+
+    }
+  
+    const user = {
+      username: "testuser"
+    }
+  
+    render(<Blog blog={blog} user={user} />)
+  
+    // Ensimmäisenä näkyy blogiotsikko ja tekijä
+    expect(screen.getByText('Moi Matti')).toBeDefined()
+
+    // fireEvent.click(screen.getByText('view'))
+
+    // Painetaan 'view' nappia käyttäen userEvent
+    const button = screen.getByText('view');
+    await userEvent.click(button); // Huomaa "await"
+    
+    // Nyt URL ja likejen määrä näkyvät
+    expect(screen.getByText('https://example.com')).toBeDefined()
+    expect(screen.getByText('likes 5')).toBeDefined()
+  })
+
+//Tee testi, joka varmistaa että myös url,
+//likejen määrä ja käyttäjä näytetään, kun blogin kaikki tiedot näyttävää nappia on painettu.
